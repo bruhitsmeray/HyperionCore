0038560d7cc2caa2f2ebc07109368f9e29f981b1
Morar Andrei - Alexandru <whiteex@icloud.com>
2022-12-16 01:23:08 +0200

    December 16th, 2022
    - No idea what I've done here xD

--- Source/HyperionCore/Private/HyperionHealthComp.cpp
+++ Source/HyperionCore/Private/HyperionHealthComp.cpp
@@ -1,16 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "HyperionHealthComp.h"
 
-// Sets default values for this component's properties
 UHyperionHealthComp::UHyperionHealthComp() {
 	PrimaryComponentTick.bCanEverTick = true;
 	MaxHealth = 100.0f;
 }
 
-
-// Called when the game starts
 void UHyperionHealthComp::BeginPlay() {
 	Super::BeginPlay();
 	AActor* Owner = GetOwner();
@@ -28,8 +24,6 @@ void UHyperionHealthComp::TakeDamage(AActor* DamagedActor, float Damage, const U
 	}
 }
 
-
-// Called every frame
 void UHyperionHealthComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	if(CurrentHealth <= 0) {
@@ -49,4 +43,3 @@ void UHyperionHealthComp::SetHealth(int Value) {
 	}
 	CurrentHealth = Value;
 }
-
