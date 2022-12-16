a42fe7aee6d9d8fd2f2defe3c8aadec55369bebc
Morar Andrei - Alexandru <whiteex@icloud.com>
2022-12-16 16:08:58 +0200

    December 16th, 2022
    - Add logging on Startup and Shutdown
    - Add a function that dynamically gets the location of the Discord files

--- Source/HyperionCore/HyperionCore.Build.cs
+++ Source/HyperionCore/HyperionCore.Build.cs
@@ -1,36 +1,25 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class HyperionCore : ModuleRules
 {
 	public HyperionCore(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				"D:\\Clones\\Decay\\Plugins\\HyperionCore\\Source\\HyperionCore\\Discord\\"
-			}
-		);
 
-		PublicAdditionalLibraries.AddRange(
-			new string[] {
-				"D:\\Clones\\Decay\\Plugins\\HyperionCore\\Binaries\\Win64\\discord_game_sdk.dll.lib"
-			}
-		);
-				
+		PublicIncludePaths.Add(GetDiscordPath);
+		PublicAdditionalLibraries.Add(GetDiscordLibFiles);
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
 			}
 		);
-			
 		
 		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				"Core",
 				"HyperionUI",
 				"KonsolePlugin",
@@ -41,8 +30,7 @@ public class HyperionCore : ModuleRules
 			
 		
 		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				"CoreUObject",
 				"Engine",
 				"Slate",
@@ -53,10 +41,54 @@ public class HyperionCore : ModuleRules
 		
 		
 		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
+			new string[] {
 				// ... add any modules that your module loads dynamically here ...
 			}
 		);
+	}
+	
+	private string GetDiscordPath {
+		get {
+			return Path.GetFullPath(Path.Combine(ModuleDirectory, "Discord"));
+		}
+	}
+	private string GetDiscordLibFiles {
+		get {
+			return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../Binaries/", GetTargetPlatform,
+				GetDiscordLabFileNames));
+		}
+	}
+	private string GetDiscordLabFileNames {
+		get {
+			if (bIsWin64()) {
+				return "discord_game_sdk.dll.lib";
+			} else if (bIsMac()) {
+				return "discord_game_sdk.dylib";
+			}
+			return null;
+		}
+	}
+	
+	private bool bIsWin64() {
+		return Target.Platform == UnrealTargetPlatform.Win64;
+	}
+	private bool bIsMac() {
+		return Target.Platform == UnrealTargetPlatform.Mac;
+	}
+	private bool bIsLinux() {
+		return Target.Platform == UnrealTargetPlatform.Linux;
+	}
+	
+	private string GetTargetPlatform {
+		get {
+			if (bIsWin64()) {
+				return "Win64";
+			} else if (bIsMac()) {
+				return "Mac";
+			} else if (bIsLinux()) {
+				return "Linux/x86_64-unknown-linux-gnu";
+			}
+			return null;
+		}
 	}
 }
--- Source/HyperionCore/Private/HyperionCore.cpp
+++ Source/HyperionCore/Private/HyperionCore.cpp
@@ -4,17 +4,28 @@
 
 #define LOCTEXT_NAMESPACE "FHyperionCoreModule"
 
+DEFINE_LOG_CATEGORY_STATIC(LogHyperionCore, All, All);
+
+#define LOG(LogString)	UE_LOG(LogHyperionCore, Display, TEXT("%s"), *FString(LogString));
+#define LOG_WARNING(LogString)	UE_LOG(LogHyperionCore, Warning, TEXT("%s"), *FString(LogString));
+#define LOG_ERROR(LogString)	UE_LOG(LogHyperionCore, Error, TEXT("%s"), *FString(LogString));
+
 void FHyperionCoreModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	LOG("Initializing HyperionCore.");
 }
 
 void FHyperionCoreModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+	LOG("Shutting down HyperionCore.");
 }
 
+#undef LOG
+#undef LOG_WARNING
+#undef LOG_ERROR
 #undef LOCTEXT_NAMESPACE
 	
 IMPLEMENT_MODULE(FHyperionCoreModule, HyperionCore)
